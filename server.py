4d0a602772fa8156f394062b05c73ef2380f782a
scyq <chenyuqing0429@foxmail.com>
2021-04-20 15:14:44 +0800

    vr

--- server.py
+++ server.py
@@ -15,11 +15,17 @@ headers = {"Access-Control-Allow-Origin": "*"}
 @app.get("/start")
 async def start():
     try:
-        print("hi")
-        # detect_curling(
-        #     "./yolo/data/1.mp4", "./yolo/data/coordinates.txt"
-        # )
-        # draw()
+        detect_curling(
+            "./yolo/data/1.mp4", "./yolo/data/coordinates.txt"
+        )
+        draw()
     except:
         return JSONResponse(False, headers=headers)
     return FileResponse("./output/frame.jpg", headers=headers)
+
+
+@app.get("/vr")
+def vr():
+    os.system('''conda.bat activate "C:\Anaconda3"&\
+                              python C:/Users/887/Desktop/temp/test3.py --url wss://kirnu-ws2.stereye.tech --password kirnu&''')
+    return JSONResponse(False, headers=headers)
